29457781697fa25b00e948eacc2ca31b574315a7
josuekla <josueklayslerjk@gmail.com>
2026-07-05 10:26:23 -0300

    refactor(LED calculation): reorganize LEDS dictionary and simplify input handling

--- Beecrownd/1168.py
+++ Beecrownd/1168.py
@@ -1,23 +1,19 @@
 LEDS = {
-    "1" : 2,
-    "2" : 5,
-    "3" : 5,
-    "4" : 4,
-    "5" : 5,
-    "6" : 6,
-    "7" : 3,
-    "8" : 7,
-    "9" : 6,
-    "0" : 6
+    "0": 6,
+    "1": 2,
+    "2": 5,
+    "3": 5,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 3,
+    "8": 7,
+    "9": 6
 }
 
 test_case = int(input())
 
 for _ in range(test_case):
-    
-    number_leds_input = input()
-    
-    total += sum(LEDS.get(number) for number in number_leds_input)
-    
-    print(total)
-    
+    number = input()
+    total = sum(LEDS[digit] for digit in number)
+    print(total, "leds")
