88e6709082802a8a592a7b61d3d2c6a47753437f
josuekla <josueklayslerjk@gmail.com>
2026-07-04 22:58:21 -0300

    refactor(LED calculation): streamline LED counting logic and remove unnecessary variables

--- Beecrownd/1168.py
+++ Beecrownd/1168.py
@@ -1,34 +1,23 @@
+LEDS = {
+    "1" : 2,
+    "2" : 5,
+    "3" : 5,
+    "4" : 4,
+    "5" : 5,
+    "6" : 6,
+    "7" : 3,
+    "8" : 7,
+    "9" : 6,
+    "0" : 6
+}
+
 test_case = int(input())
 
-total_leds = []
-
-while test_case > 0:
+for _ in range(test_case):
     
-    mappers_numbers = {
-        "1" : 2,
-        "2" : 5,
-        "3" : 5,
-        "4" : 4,
-        "5" : 5,
-        "6" : 6,
-        "7" : 3,
-        "8" : 7,
-        "9" : 6,
-        "0" : 6
-    }
     number_leds_input = input()
     
-    total_leds_individual = 0
-    for number in number_leds_input:
-        if number in mappers_numbers.keys():
-            total_leds_individual += mappers_numbers.get(number)
-            
+    total += sum(LEDS.get(number) for number in number_leds_input)
     
-    total_leds.append(total_leds_individual)
+    print(total)
     
-    
-    test_case -= 1
-    
-    
-for numbers_leds in total_leds:
-    print(numbers_leds, "leds") 
